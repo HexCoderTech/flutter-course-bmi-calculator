24887346f9e5276e8e79bb32903e4570119118b0
Praneeth Peiris <gnomez.grave@gmail.com>
2024-05-01 10:08:34 +0200

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFF090C20);
+const kTileColor = Color(0xFF1D1F33);
+const kSelectedTileColor = Color.fromARGB(255, 42, 29, 51);
+const kActiveTextColor = Color(0xFFFFFFFF);
+const kInactiveTextColor = Color(0xFF8D8E98);
+const kScaleButtonColor = Color.fromARGB(255, 41, 42, 73);
+const kButtonColor = Color(0xFFEC1356);
+var kTileBorderDecoration = BoxDecoration(
+  color: kTileColor,
+  borderRadius: BorderRadius.circular(5),
+);
+
+var kSelectedTileBorderDecoration = BoxDecoration(
+  color: kSelectedTileColor,
+  borderRadius: BorderRadius.circular(5),
+);
